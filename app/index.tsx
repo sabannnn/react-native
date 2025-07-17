a20a8c6c9ef95b3b2e7706a90b317b84d6987328
Sabannnn <105841110322@student.unismuh.ac.id>
2025-07-17 12:10:00 +0800

    update

--- app/index.tsx
+++ app/index.tsx
@@ -8,39 +8,19 @@ import {
 } from "react-native";
 
 const imageData = [
-  { id: '1', primary: 'https://i.pinimg.com/736x/5c/db/09/5cdb09c55bceddbe99670372780546ae.jpg',
-    alternate: 'https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/p1/408/2025/04/18/30151-17448211994315-1920-4226333009.jpg'
-  },
-  { id: '2', primary: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfkn5bBQzskYbpmlsXij2M58vwNC9okifkCQ&s',
-    alternate: 'https://cdnwpseller.gramedia.net/wp-content/uploads/2023/02/roronoza-zoro.webp'
-  },
-  { id: '3', primary: 'https://i.pinimg.com/736x/88/2f/e7/882fe729340b6d27ff3f397e594c3f5d.jpg',
-    alternate: 'https://assets.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2023/01/05/1731101867.jpg'
-  },
-  { id: '4', primary: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsF5m9Jr0fgOFNNfwfWXQRInjkO_IJkPLavw&s',
-    alternate: 'https://i.pinimg.com/736x/19/fb/aa/19fbaa5fc273d44b56f2dee1dd270f81.jpg'
-  },
-  { id: '5', primary: 'https://i.pinimg.com/736x/81/7e/a5/817ea5ea5fbaf170065fcfb312f50dff.jpg',
-    alternate: 'https://assets.jabarekspres.com/sukabumi/2023/12/Usopp.png'
-  },
-  { id: '6', primary: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGmB3zHWx4URjmsu7RietyY8oP5OP8huGL9Q&s',
-    alternate: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAtzb6mC3IEdTfTgor-15kzMw6C9Y1I0vkrw&s'
-  },
-  { id: '7', primary: 'https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/p1/854/2024/12/23/Nico-Robin-Kid-young-One-Piece-3001585893.jpg',
-    alternate: 'https://cdn.rri.co.id/berita/Bengkulu/o/1736514139954-nico-robin-and-poneglyph/f9fu7pog1p8mh0u.jpeg'
-  },
-  { id: '8', primary: 'https://i.pinimg.com/736x/36/7b/18/367b18a117159121cfd90a10bf91d258.jpg',
-    alternate: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOo6IC2tFhEWKdftQgckfz-84u7iE1JrKDvQ&s'
-  },
-  { id: '9', primary: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSco9vE3AJCcRJo3qaM4fyixdfNPWopoEj438AZ66zQgIPFSRf_Bcn2qqj6I4tA7AZyDPQ&usqp=CAU',
-    alternate: 'https://image.ggwp.id/post/20250306/teknikjinbe5.jpg'
-  },
+  { id: '1', primary: 'https://i.pinimg.com/736x/5c/db/09/5cdb09c55bceddbe99670372780546ae.jpg', alternate: 'https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/p1/408/2025/04/18/30151-17448211994315-1920-4226333009.jpg' },
+  { id: '2', primary: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfkn5bBQzskYbpmlsXij2M58vwNC9okifkCQ&s', alternate: 'https://cdnwpseller.gramedia.net/wp-content/uploads/2023/02/roronoza-zoro.webp' },
+  { id: '3', primary: 'https://i.pinimg.com/736x/88/2f/e7/882fe729340b6d27ff3f397e594c3f5d.jpg', alternate: 'https://assets.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/2023/01/05/1731101867.jpg' },
+  { id: '4', primary: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQsF5m9Jr0fgOFNNfwfWXQRInjkO_IJkPLavw&s', alternate: 'https://i.pinimg.com/736x/19/fb/aa/19fbaa5fc273d44b56f2dee1dd270f81.jpg' },
+  { id: '5', primary: 'https://i.pinimg.com/736x/81/7e/a5/817ea5ea5fbaf170065fcfb312f50dff.jpg', alternate: 'https://assets.jabarekspres.com/sukabumi/2023/12/Usopp.png' },
+  { id: '6', primary: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRGmB3zHWx4URjmsu7RietyY8oP5OP8huGL9Q&s', alternate: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAtzb6mC3IEdTfTgor-15kzMw6C9Y1I0vkrw&s' },
+  { id: '7', primary: 'https://static.promediateknologi.id/crop/0x0:0x0/750x500/webp/photo/p1/854/2024/12/23/Nico-Robin-Kid-young-One-Piece-3001585893.jpg', alternate: 'https://cdn.rri.co.id/berita/Bengkulu/o/1736514139954-nico-robin-and-poneglyph/f9fu7pog1p8mh0u.jpeg' },
+  { id: '8', primary: 'https://i.pinimg.com/736x/36/7b/18/367b18a117159121cfd90a10bf91d258.jpg', alternate: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOo6IC2tFhEWKdftQgckfz-84u7iE1JrKDvQ&s' },
+  { id: '9', primary: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSco9vE3AJCcRJo3qaM4fyixdfNPWopoEj438AZ66zQgIPFSRf_Bcn2qqj6I4tA7AZyDPQ&usqp=CAU', alternate: 'https://image.ggwp.id/post/20250306/teknikjinbe5.jpg' },
 ];
 
-const screenWidth = Dimensions.get("window").width;
-const margin = 5;
-const numColumns = 3;
 const CELL_SIZE = 80;
+const numColumns = 3;
 
 export default function ImageGrid() {
   const [states, setStates] = useState(
@@ -63,9 +43,10 @@ export default function ImageGrid() {
     );
   };
 
+  // Membagi gambar menjadi 3 baris
   const rows = [];
-  for (let i = 0; i < imageData.length; i += 3) {
-    rows.push(imageData.slice(i, i + 3));
+  for (let i = 0; i < imageData.length; i += numColumns) {
+    rows.push(imageData.slice(i, i + numColumns));
   }
 
   return (
@@ -73,7 +54,7 @@ export default function ImageGrid() {
       {rows.map((row, rowIdx) => (
         <View key={rowIdx} style={styles.row}>
           {row.map((image, colIdx) => {
-            const index = rowIdx * 3 + colIdx;
+            const index = rowIdx * numColumns + colIdx;
             return (
               <Pressable
                 key={image.id}
