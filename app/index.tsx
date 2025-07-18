184c6ee498ec8694c60b0ff1e0b2193fb98de137
Sabannnn <105841110322@student.unismuh.ac.id>
2025-07-19 03:35:07 +0800

    update

--- app/index.tsx
+++ app/index.tsx
@@ -13,10 +13,9 @@ export default function IndexPage() {
       }
     <Text style={styles.latoBlack}>105841109722 MUH. FADIL AHMAD</Text><Text style={styles.manrope}>105841109822 DAYANG AISYAH</Text>
     <Text style={styles.interItalic}>105841109922 USRAN</Text><Text style={styles.firaCode}>105841100122 MUH. ZAIN HIDAYAT</Text>
-    <Text style={styles.montserratBold}>105841110222 ILFAUZA FEBRIANTY FAISAL</Text><Text style={styles.ralewayItalic}>105841110322 SABAN</Text><Text style={styles.mySoul}>105841110422 NURFADILA SARI</Text>
+    <Text style={styles.montserratBold}>105841110222 ILFAUZA FEBRIANTY FAISAL</Text><Text style={styles.mySoul}>105841110422 NURFADILA SARI</Text>
     <Text style={styles.openSansItalic}>105841110522 MUH. HIJRIL ILMAN</Text><Text style={styles.robotoItalic}>105841110622 WA NANDA SULISTRIAN</Text>
-    <Text style={styles.oswald}>105841110722 MUH. TEGAR ALFIKRI</Text>
-    </ScrollView>
+    <Text style={styles.oswald}>105841110722 MUH. TEGAR ALFIKRI</Text><Text style={styles.ralewayItalic}>105841110822 RAIHANATUL JANNAH</Text></ScrollView>
   );
 }
 
